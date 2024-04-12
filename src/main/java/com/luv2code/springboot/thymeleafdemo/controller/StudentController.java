727ead9c519eb7a8bba83c4fbbe92fc8fc8cced0
nkserveren26 <norinoritoshi@outlook.jp>
2024-04-12 15:50:22 +0900

    Studentクラス追加。

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+
+        // create a student object
+        Student theStudent = new Student();
+
+        // add student object to the model
+        theModel.addAttribute("student", theStudent);
+        System.out.println(theModel.toString());
+        return "student-form";
+    }
+}

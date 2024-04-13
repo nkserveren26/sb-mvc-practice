8ead897051f5abfc5d5436a5058c09081aee8ed4
nkserveren26 <norinoritoshi@outlook.jp>
2024-04-13 21:54:53 +0900

    StudentFormにドロップダウンリスト追加 propertiesファイルでリスト指定

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -19,6 +25,10 @@ public class StudentController {
         // add student object to the model
         theModel.addAttribute("student", theStudent);
         System.out.println(theModel.toString());
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
         return "student-form";
     }
 

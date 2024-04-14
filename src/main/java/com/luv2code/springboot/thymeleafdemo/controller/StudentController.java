06fd735793f94d92056603d4791629f6518736e1
nkserveren26 <norinoritoshi@outlook.jp>
2024-04-14 16:49:12 +0900

    StudentFormにラジオボタン追加。 propertiesファイルでラジオボタンのリスト指定。

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -16,6 +16,9 @@ public class StudentController {
     @Value("${countries}")
     private List<String> countries;
 
+    @Value("${languages}")
+    private List<String> languages;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -28,6 +31,9 @@ public class StudentController {
 
         // add the list of countries to the model
         theModel.addAttribute("countries", countries);
+
+        // add the list of languages to the model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }
